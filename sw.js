9889b8d6712c2bddd1bc1d252e480373fbcbc448
gabrielchowaniec0-svg <gabrielchowaniec0@gmail.com>
2026-08-11 12:18:43 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+  console.log('[Service Worker] Install');
 });
 
 self.addEventListener('fetch', (e) => {
-    // Basic pass-through fetch for PWA requirement
-    e.respondWith(fetch(e.request));
+  // Basic pass-through fetch for PWA requirement
+  e.respondWith(fetch(e.request));
 });
